82ed54fd0e35f28bea6da3528bd75acacb5365a2
Elias Jørgensen <eliaszakarias@gmail.com>
2019-02-19 20:52:16 +0100

    WIP

--- app/src/main/java/io/ezjay/bikeshare/BikeShareActivity.kt
+++ app/src/main/java/io/ezjay/bikeshare/BikeShareActivity.kt
@@ -1,53 +1,17 @@
 package io.ezjay.bikeshare
 
-import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.ListView
-import io.ezjay.bikeshare.adapter.RideArrayAdapter
-import io.ezjay.bikeshare.data.RidesDb
+import android.support.v7.app.AppCompatActivity
 
 class BikeShareActivity : AppCompatActivity() {
-
-    // UI
-    private lateinit var addRide : Button
-    private lateinit var endRide : Button
-    private lateinit var listRides : Button
-    private lateinit var list : ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bike_share)
 
-        this.addRide = this.findViewById(R.id.add_button)
-        this.endRide = this.findViewById(R.id.end_button)
-        this.listRides = this.findViewById(R.id.list_button)
-
-        this.addRide.setOnClickListener {
-            val intent = Intent(this,  StartRideActivity::class.java)
-            this.startActivity(intent)
-        }
-
-        this.endRide.setOnClickListener {
-            val intent = Intent(this, EndRideActivity::class.java)
-            this.startActivity(intent)
-        }
-
-        this.listRides.setOnClickListener {
-            if (this.list.visibility == View.GONE)
-                this.list.visibility = View.VISIBLE
-            else
-                this.list.visibility = View.GONE
-        }
-
-        this.list = this.findViewById(R.id.ride_list)
-        this.list.adapter = RideArrayAdapter(this, RidesDb.getRides())
-    }
-
-    override fun onStart() {
-        super.onStart()
-        this.list.adapter = RideArrayAdapter(this, RidesDb.getRides())
+        val fm = this.supportFragmentManager
+        fm.beginTransaction()
+            .replace(R.id.fragment_container, BikeShareFragment())
+            .commit()
     }
 }
---
+++ app/src/main/java/io/ezjay/bikeshare/BikeShareFragment.kt
@@ -0,0 +1,44 @@
+package io.ezjay.bikeshare
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.View
+import android.widget.Button
+import android.widget.ListView
+import io.ezjay.bikeshare.adapter.RideArrayAdapter
+import io.ezjay.bikeshare.data.RidesDb
+
+class BikeShareFragment : Fragment() {
+    // UI
+    private lateinit var addRide : Button
+    private lateinit var endRide : Button
+    private lateinit var listRides : Button
+    private lateinit var list : ListView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        this.addRide = view.findViewById(R.id.add_button)
+        this.endRide = view.findViewById(R.id.end_button)
+        this.listRides = view.findViewById(R.id.list_button)
+
+        this.addRide.setOnClickListener {
+            val intent = Intent(view.context,  StartRideActivity::class.java)
+            this.startActivity(intent)
+        }
+
+        this.endRide.setOnClickListener {
+            val intent = Intent(view.context, EndRideActivity::class.java)
+            this.startActivity(intent)
+        }
+
+        this.listRides.setOnClickListener {
+            if (this.list.visibility == View.GONE)
+                this.list.visibility = View.VISIBLE
+            else
+                this.list.visibility = View.GONE
+        }
+
+        this.list = view.findViewById(R.id.ride_list)
+        this.list.adapter = RideArrayAdapter(view.context, RidesDb.getRides())
+    }
+}
